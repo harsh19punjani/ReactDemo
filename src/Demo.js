a3622268c316dd1a5c2b8a0b04c9ae3e4f2b991c
harsh19punjani <punjaniharsh@gmail.com>
2020-07-28 17:36:32 +0530

    State commit

--- src/Demo.js
+++ src/Demo.js
@@ -4,26 +4,40 @@ import React, {
 import './Demo.css';
 import ReactDOM from 'react-dom';
 
-//class type component
-// class Demo extends Component {
-//     render() {
-//         return <div className='main'>
-//         {/* <div class='main'>  can't write class javascript key work .... read has className */}  
-//             <h4> Demo Component is loaded.. </h4>
-//             <p>  Position: {this.props.position}</p>
-//         </div>
+class Demo extends Component {
+    constructor() {
+        super();
+        this.State = {
+            name: "from state"
+        }
        
-//     }
-// }
+    }
+ 
+    click = () => {
+            this.setState({ name: 'new state value' }, () => 
+            console.log(this.state.name));
+    }
+    
+
+    render() {
+        return <div className='main'>
+            {/* <div class='main'>  can't write class javascript key work .... read has className */}
+            <h4> {this.State.name} </h4>
+            <p>  Position: {this.props.position}</p>
+            <button onClick={this.click}>click to change</button>
+        </div>
+
+    }
+}
 
 // function type component
 
-const Demo = (props) =>{
-    return <div className='main'>
-    {/* <div class='main'>  can't write class javascript key work .... read has className */}  
-        <h4> Demo Component is loaded.. </h4>
-        <p>  Position: {props.position}</p>
-    </div>
-}
+// const Demo = (props) =>{
+//     return <div className='main'>
+//     {/* <div class='main'>  can't write class javascript key work .... read has className */}  
+//         <h4> Demo Component is loaded.. </h4>
+//         <p>  Position: {props.position}</p>
+//     </div>
+// }
 
 export default Demo;
