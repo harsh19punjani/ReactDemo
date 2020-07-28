058dccad5cd72f0659288eea8627388881c83b2e
harsh19punjani <punjaniharsh@gmail.com>
2020-07-28 15:02:20 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Demo from './Demo';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Demo position='Senior Software developer' />  {/*component name */}
+    {/* <App />  */}
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
