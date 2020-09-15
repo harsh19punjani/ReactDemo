f84ba077966ab8e2231b717cc3014fd8345a4787
harsh19punjani <punjaniharsh@gmail.com>
2020-09-15 18:46:05 +0530

    integration of bootstrap in react proejct

--- src/Demo.js
+++ src/Demo.js
@@ -2,27 +2,24 @@ import React, {
     Component
 } from 'react';
 import './Demo.css';
-import ReactDOM from 'react-dom';
-
-//class type component
-// class Demo extends Component {
-//     render() {
-//         return <div className='main'>
-//         {/* <div class='main'>  can't write class javascript key work .... read has className */}  
-//             <h4> Demo Component is loaded.. </h4>
-//             <p>  Position: {this.props.position}</p>
-//         </div>
-       
-//     }
-// }
 
 // function type component
 
-const Demo = (props) =>{
-    return <div className='main'>
-    {/* <div class='main'>  can't write class javascript key work .... read has className */}  
-        <h4> Demo Component is loaded.. </h4>
-        <p>  Position: {props.position}</p>
+const Demo = (props) => {
+    return <div >
+       <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
     </div>
 }
 
